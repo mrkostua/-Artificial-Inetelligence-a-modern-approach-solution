5f5a480aaf429e328cd565a499908c5f6b3b946d
Kostiantyn Prysiazny <kprysiazhnyi@gmail.com>
2018-02-03 16:21:04 +0100

    Update EnvironmentObject.kt

--- EnvironmentObject.kt
+++ EnvironmentObject.kt
@@ -1,5 +1,3 @@
-package com.mrkostua.mathalarm
-
 import java.util.*
 
 /**
@@ -7,10 +5,12 @@ import java.util.*
  */
 class EnvironmentObject(private val dimensionX: Int, private val dimensionY: Int, private val percentOfDirt: Int) {
     private val random = Random()
+    private val amountOfRooms = dimensionX * dimensionY
     var currentEnvironment: MutableMap<Pair<Int, Int>, Boolean>
     var currentAmountOfDirtyRooms: Int
     var positionOfCleaner: Pair<Int, Int>
-
+	
+    
     init {
         currentEnvironment = generateInitialEnvironment()
         currentAmountOfDirtyRooms = getInitialAmountOfDirt()
@@ -20,9 +20,31 @@ class EnvironmentObject(private val dimensionX: Int, private val dimensionY: Int
 
     public fun isCurrentRoomClean(): Boolean = currentEnvironment[positionOfCleaner] ?: true
 
-    private fun generateInitialEnvironment(): SortedMap<Pair<Int, Int>, Boolean> {
-
-        TODO("randomly generate some 60% of dirty rooms in [][]")
+    private fun generateInitialEnvironment(): MutableMap<Pair<Int, Int>, Boolean> {
+		//first of all we need to intial some size of enviroment in our app it can only x/y enviroment with walls and without any obstacles
+		//also we need in some way generate percentOfDirt and randomly set it in environment
+		val environmentMap = MutableMap<Pair<Int,Int>, Boolean>()
+        for(x in 0..dimensionX){
+            for(y in 0..dimensionY){
+                environmentMap.put(Pair(x,y),false)
+            }
+        }
+       // use method genareteRandomlyDirtyRooms to set random rooms as dirty rooms
+        
+        return environmentMap
+    }
+   private val dirtyRoomsLocationList : MutableSet<Pair<Int,Int>> = HashSet<Pair<Int,Int>>() 
+    private fun genareteRandomlyDirtyRooms() {
+        //here we need to randomly generate some amount of dirty rooms Pair(int,int) and every new generated room need to be unique so we can generate precisely given amount of dirty rooms
+   		     val requiredAmountOfDirt = getInitialAmountOfDirt()
+       var dirtyRoomLocation : Pair<Int,Int> = Pair<0,0>
+        	while(i<requiredAmountOfDirt){
+                dirtRoomLocation = Pair(getRandomNumber(0, dimensionX), getRandomNumber(0, dimensionY))
+                if(!dirtyRoomsLocationList.contains(dirtRoomLocation)){
+                    dirtyRoomsLocationList.put(dirtRoomLocation)
+                    i++
+                }
+            }
     }
 
     private fun getInitialAmountOfDirt(): Int = (dimensionX * dimensionY * percentOfDirt) / 100
